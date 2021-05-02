93e77c2b5675b7f6306631a28a43ad8009a38cf1
MX_Master <mikhail@vydrenko.ru>
2021-05-02 15:38:11 +0600

    + donate.html

---
+++ img/donate.js
@@ -0,0 +1,8 @@
+document.writeln("<h2 style='color:#555'>");
+document.writeln((3381-11+3658-1859));
+document.writeln(7645+5542-8475+259);
+document.writeln("81"+"<span style='display:none'>62</span>"+"4"+(10+3-7-6));
+document.writeln("<span class='d1'></span>");
+document.writeln("</h2>");
+document.writeln(String.fromCharCode(0x4D)+"IKH"+"AI"+""+"L");
+document.writeln("V"+"YD"+String.fromCharCode(0x52)+"EN"+"KO");
