a365455744e8186c223f43be8d060d345d4e7e0e
MX_Master <mikhail@vydrenko.ru>
2021-05-03 10:03:07 +0600

    + another payment method

--- img/donate.js
+++ img/donate.js
@@ -4,5 +4,9 @@ document.writeln(7645+5542-8475+259);
 document.writeln("81"+"<span style='display:none'>62</span>"+"4"+(10+3-7-6));
 document.writeln("<span class='d1'></span>");
 document.writeln("</h2>");
+
 document.writeln(String.fromCharCode(0x4D)+"IKH"+"AI"+""+"L");
 document.writeln("V"+"YD"+String.fromCharCode(0x52)+"EN"+"KO");
+
+document.writeln("<br/><br/>OR use this form<br/>");
+document.writeln('<h2><iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=donation&targets-hint=&default-sum=&button-text=14&payment-type-choice=on&mobile-payment-type-choice=on&comment=on&hint=&successURL=&quickpay=shop&account=41001691842214" width="100%" height="301" frameborder="0" allowtransparency="true" scrolling="no"></iframe></h2>');
