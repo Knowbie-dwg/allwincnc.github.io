0901f62d61f5ca642c692c80b7babc86fdab0af8
MX_Master <mikhail@vydrenko.ru>
2021-05-07 17:14:37 +0600

    * project name update

--- img/menu.js
+++ img/menu.js
@@ -1,10 +1,10 @@
 document.writeln(
     '<div class="menu">'+
-        '<a href="index.html" class="name">h3cnc</a>'+
+        '<a href="index.html" class="name">allwincnc</a>'+
         ' &middot; <a href="install.html">Install</a>'+
         ' &middot; <a href="wiring.html">Wiring</a>'+
         ' &middot; <a href="docs.html">Docs</a>'+
-        ' &middot; <a href="https://github.com/h3cnc">Sources</a>'+
+        ' &middot; <a href="https://github.com/allwincnc">Sources</a>'+
         ' &middot; <a href="https://forum.linuxcnc.org/18-computer/39037-linuxcnc-orange-pi">Forum&nbsp;(EN)</a>'+
         ' &middot; <a href="https://www.cnc-club.ru/forum/viewtopic.php?f=41&t=16788">Forum&nbsp;(RU)</a>'+
     '</div>'+
