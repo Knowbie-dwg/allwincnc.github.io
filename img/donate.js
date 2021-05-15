316e009138a7c570cb5461ae7f1f526ae425f0bb
MX_Master <mikhail@vydrenko.ru>
2021-05-15 12:02:56 +0600

    * YM id update

--- img/donate.js
+++ img/donate.js
@@ -1,5 +1,5 @@
 document.writeln('<h3><br/>');
 document.writeln('&middot; <a href="https://paypal.me/mikhailvydrenko">PAYPAL</a>');
 document.writeln(' &middot; ');
-document.writeln('<a href="https://yoomoney.ru/to/41001691842214">YOOMONEY</a> &middot;');
+document.writeln('<a href="https://yoomoney.ru/to/4100116780122709">YOOMONEY</a> &middot;');
 document.writeln('<br/></h3>');
