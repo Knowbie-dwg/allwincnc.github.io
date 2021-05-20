71ce34e7bc42adbd335293b65dad9352760d882a
MX_Master <mikhail@vydrenko.ru>
2021-05-20 18:45:34 +0600

    * link names fix

--- img/donate.js
+++ img/donate.js
@@ -1,5 +1,5 @@
 document.writeln('<h3><br/>');
-document.writeln('&middot; <a href="https://paypal.me/mikhailvydrenko">PAYPAL</a>');
-document.writeln(' &middot; ');
-document.writeln('<a href="https://yoomoney.ru/to/4100116780122709">YOOMONEY</a> &middot;');
+document.writeln('<a href="https://yoomoney.ru/to/4100116780122709">Яндекс.Деньги</a>');
+document.writeln('<br/><br/>&middot; &middot; &middot; <br/><br/>');
+document.writeln('<a href="https://paypal.me/mikhailvydrenko">PAYPAL</a>');
 document.writeln('<br/></h3>');
