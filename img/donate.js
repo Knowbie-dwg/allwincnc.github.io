85a07f7e6f516e0902e4708d8b1b49ab63a2bef1
MX_Master <mikhail@vydrenko.ru>
2021-06-09 15:56:57 +0600

    * removed f**ng paypal

--- img/donate.js
+++ img/donate.js
@@ -1,5 +1,3 @@
 document.writeln('<h3><br/>');
-document.writeln('<a href="https://yoomoney.ru/to/4100116780122709">Яндекс.Деньги</a>');
-document.writeln('<br/><br/>&middot; &middot; &middot; <br/><br/>');
-document.writeln('<a href="https://paypal.me/mikhailvydrenko">PAYPAL</a>');
+document.writeln('<a href="https://yoomoney.ru/to/4100116780122709">Яндекс.Деньги</a> (yoomoney)');
 document.writeln('<br/></h3>');
