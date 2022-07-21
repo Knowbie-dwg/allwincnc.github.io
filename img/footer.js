04c4f762fd4c3bf8fdfc654711761412fe93b485
MX_Master <64367709+MX-Master@users.noreply.github.com>
2022-07-21 06:46:13 +0600

    - support email

--- img/footer.js
+++ img/footer.js
@@ -1,4 +1,3 @@
 document.writeln('<div class="footer">');
-document.writeln('any questions?');
-document.writeln('<a href="mai'+String.fromCharCode(0x6C)+'to'+":"+'mikh'+'ail'+String.fromCharCode(0x40)+'v'+'ydrenko'+String.fromCharCode(0x2E)+'ru">mi'+String.fromCharCode(0x6B)+'hail'+String.fromCharCode(0x40)+'vyd'+'ren'+'ko'+String.fromCharCode(0x2E)+'ru</a>');
+document.writeln('There is no supoort for the project, use it on your own risk ;)');
 document.writeln('</div>');
